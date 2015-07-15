a78dc0c8e62367584835a3fdc96a1058bc9641fd
sreenekanth <sreenekanth26@gmail.com>
2015-07-15 15:15:48 -0400

    Directive added

--- app/directive/button.directive.js
+++ app/directive/button.directive.js
@@ -5,16 +5,15 @@
 	  .directive('wt-button', function() {
 	  return {
 		restrict: 'E',
-		transclude:true;
-		template:<button type="submit"> </button>
+		transclude:true,
+		template:'<button type="submit"> </button>',
 		link:function(scope,element,attrs)
 		{
-			element.bind("onclick",function(col){
-				id.style.backgroundColor:red;
+			element.bind("onclick",function(){
+				body.style.backgroundColor:red
 				
 			});
-		}
-	  };
+		
 	});
 
 })();
